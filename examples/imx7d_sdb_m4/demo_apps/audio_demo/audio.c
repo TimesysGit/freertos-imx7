018ede43f0fad9354cb9513c1a3a54a66bc0fc87
Justin Waters <justin.waters@timesys.com>
2016-05-10 17:14:41 -0400

    imx7d_sdb_m4: audio_demo: Add audio processing framework

---
+++ examples/imx7d_sdb_m4/demo_apps/audio_demo/audio.c
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2016, Timesys Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of Freescale Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//  Includes
+///////////////////////////////////////////////////////////////////////////////
+#include "FreeRTOS.h"
+#include "board.h"
+#include "debug_console_imx.h"
+#include "sai_imx7.h"
+#include "audio.h"
+
+/* Note: Nothing is done with this yet */
+static sai_init_config_t saiInitConfig = {
+    .clockRate = 12288000,
+};
+
+enum channel {
+    RIGHT = 0,
+    LEFT = 1,
+};
+
+static volatile uint32_t samp_in[2] = {0, 0};
+static volatile uint32_t samp_out[2] = {0, 0};
+
+////////////////////////////////////////////////////////////////////////////////
+// Code
+////////////////////////////////////////////////////////////////////////////////
+
+#ifdef __DEBUG
+void audio_dump_reg()
+{
+        uint32_t printsamp[4];
+
+        printsamp[0] = samp_in[LEFT];
+        printsamp[1] = samp_in[RIGHT];
+        printsamp[2] = samp_out[LEFT];
+        printsamp[3] = samp_out[RIGHT];
+
+        PRINTF("TCSR = %08x\n\r", I2S_TCSR_REG(BOARD_I2S_BASEADDR));
+        PRINTF("TCR1 = %08x\n\r", I2S_TCR1_REG(BOARD_I2S_BASEADDR));
+        PRINTF("TCR2 = %08x\n\r", I2S_TCR2_REG(BOARD_I2S_BASEADDR));
+        PRINTF("TCR3 = %08x\n\r", I2S_TCR3_REG(BOARD_I2S_BASEADDR));
+        PRINTF("TCR4 = %08x\n\r", I2S_TCR4_REG(BOARD_I2S_BASEADDR));
+        PRINTF("TCR5 = %08x\n\r", I2S_TCR5_REG(BOARD_I2S_BASEADDR));
+        PRINTF("RCSR = %08x\n\r", I2S_RCSR_REG(BOARD_I2S_BASEADDR));
+        PRINTF("RCR1 = %08x\n\r", I2S_RCR1_REG(BOARD_I2S_BASEADDR));
+        PRINTF("RCR2 = %08x\n\r", I2S_RCR2_REG(BOARD_I2S_BASEADDR));
+        PRINTF("RCR3 = %08x\n\r", I2S_RCR3_REG(BOARD_I2S_BASEADDR));
+        PRINTF("RCR4 = %08x\n\r", I2S_RCR4_REG(BOARD_I2S_BASEADDR));
+        PRINTF("RCR5 = %08x\n\r", I2S_RCR5_REG(BOARD_I2S_BASEADDR));
+        PRINTF("RFR  = %08x\n\r", I2S_RFR_REG(BOARD_I2S_BASEADDR,0));
+        PRINTF("RMR  = %08x\n\r", I2S_RMR_REG(BOARD_I2S_BASEADDR));
+
+        PRINTF("samp_in[LEFT] = 0x%08x\n\r", printsamp[0]);
+        PRINTF("samp_in[RIGHT] = 0x%08x\n\r", printsamp[1]);
+        PRINTF("samp_out[LEFT] = 0x%08x\n\r", printsamp[2]);
+        PRINTF("samp_out[RIGHT] = 0x%08x\n\r", printsamp[3]);
+        PRINTF("\n\r");
+}
+#else
+void audio_dump_reg() {};
+#endif
+
+void audio_init()
+{
+
+    SAI_Init(BOARD_I2S_BASEADDR, &saiInitConfig);
+
+    /* Set SAI Interrupt priority */
+    NVIC_SetPriority(BOARD_I2S_IRQ_NUM, 3);
+
+    /* Call core API to enable the IRQ. */
+    NVIC_EnableIRQ(BOARD_I2S_IRQ_NUM);
+
+    SAI_Enable(BOARD_I2S_BASEADDR);
+}
+
+/* All audio processing should occur in this function */
+static void ProcessAudio()
+{
+    samp_out[LEFT] = samp_in[LEFT];
+    samp_out[RIGHT] = samp_in[RIGHT];
+}
+
+void BOARD_I2S_HANDLER(void)
+{
+
+    SAI_ClearIRQ(BOARD_I2S_BASEADDR);
+
+    /* Write samples */
+    SAI_TxData(BOARD_I2S_BASEADDR, samp_out[LEFT]);
+    SAI_TxData(BOARD_I2S_BASEADDR, samp_out[RIGHT]);
+
+    /* Read samples */
+    samp_in[LEFT] = SAI_RxData(BOARD_I2S_BASEADDR);
+    samp_in[RIGHT] = SAI_RxData(BOARD_I2S_BASEADDR);
+
+    ProcessAudio();
+}
+
+/*******************************************************************************
+ * EOF
+ ******************************************************************************/
---
+++ examples/imx7d_sdb_m4/demo_apps/audio_demo/audio.h
@@ -0,0 +1,3 @@
+
+void audio_dump_reg();
+void audio_init();
